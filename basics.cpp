61e1d11e6c0245dc917e1a318b35473bc18731dc
rushil-latawa <rushillatawa@gmail.com>
2025-07-03 15:13:33 +0530

    Add files via upload

---
+++ basics.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "my name is rushil" << endl;
+    return 0;
+} 
+
+
+// code for having output in next line
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"hello";
+//     cout<<endl;
+//     cout<<"wassup";
+// }  
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x;
+//     cin>>x;
+//     int y=x*3.14;
+//     cout<<y;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x,y,z;
+//     cin>>x >>y >>z;
+//     cout<< x*y*z;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x;
+//     cin>>x;
+//     if(x%2==0){
+//         cout<<"even";
+//     }
+//     else if(x%2!=0){
+//         cout<<"odd";
+//     }
+// }
+// #include<iostream>
+// #include<stdlib.h>
+// using namespace std;
+// int main(){
+//     int x;
+//     cin>>x;
+//     cout<<abs(x); 
+
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n,c;
+//     cin>>n;
+//     int a[n];
+//     for(int i =0;i<n;i++){
+//         cin>>a[i];
+//     }
+//     for(int i=0;i<n;i++){
+//         for(int j=i+1;j<n;j++){
+//             if(a[i]>a[j]){
+//                 c=a[i];
+//                 a[i]=a[j];
+//                 a[j]=c;
+//             }  
+//         }
+//         cout<<a[i]<<" ";
+//     }
+// }
+
+
+
+// #include<iostream>
+// using namespace std; 
+// int main(){
+//     int x,sum=1;
+//     cin>>x;
+//     int a[x];
+//     for(int i=0;i<x;i++){
+//         cin>>a[i];
+//     }
+//     for(int i=0;i<x;i++){
+//         sum*=a[i];     
+//     }             
+//     cout<<sum;
+// }
+                     
